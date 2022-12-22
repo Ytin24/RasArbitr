654b82c3d759b242634e39677005083a12f13327
артём лепаев <lepaew228@gmail.com>
2022-12-22 10:50:46 +0300

    Wpf

---
+++ RasArbitrWpf/ViewModel/MainWindowVM.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RasArbitrWPF.ViewModel;
+
+public class MainWindowVM : ViewModel
+{
+    public delegate void CloseWindow();
+
+    public event CloseWindow Close;
+   
+    
+    private WindowState _windowState = WindowState.Normal;
+    
+    public WindowState WindowState
+    {
+        get => _windowState;
+        set => Set(ref _windowState, value);
+    }
+    private void TitleBarButtons_Click(object Name)
+    {
+        switch (Name.ToString)
+        {
+            case "Exit":
+                Close.Invoke();
+                break;
+            case "Unwrap":
+                if(WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
+                else WindowState = WindowState.Maximized;
+                break;
+            case "Minimize":
+                WindowState = WindowState.Minimized;
+                break;
+            
+        }
+    }
+
+}
