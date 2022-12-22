3000a4c8a78ad51d378d6dbf2fe5157a478e8250
артём лепаев <lepaew228@gmail.com>
2022-12-22 10:50:46 +0300

    Wpf

---
+++ RasArbitrWpf/ViewModel/ViewModel.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RasArbitrWPF.ViewModel;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
