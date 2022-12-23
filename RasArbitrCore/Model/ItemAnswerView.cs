aa83a6a2b041e2759911ad223460427b0a325b12
LeeKoldun <leadkoldun@gmail.com>
2022-12-23 13:21:03 +0300

    Вид объектов

--- RasArbitrCore/Model/ItemAnswerView.cs
+++ RasArbitrCore/Model/ItemAnswerView.cs
@@ -10,14 +10,19 @@ namespace RasArbitrCore.Model {
         public Uri CaseId { get; set; } // url
         public Uri FileName { get; set; } //file url
         public string File { get; set; }
-        public String Type { get; set; }
         public String Id { get; set; }
+
+        public string CourtName { get; set; }
+        public String Type { get; set; }
+
         public ItemAnswerView(PostResult.ItemData Data) {
             CaseId= new($"https://kad.arbitr.ru/Card/{Data.CaseId}");
             File = Data.FileName;
             FileName = new($"https://kad.arbitr.ru/Document/Pdf/{Data.CaseId}/{Data.Id}/{Data.FileName}");
-            Type = Data.Type;
             Id = Data.Id;
+
+            CourtName = Data.Court;
+            Type = Data.Type;
         }
 
     }
