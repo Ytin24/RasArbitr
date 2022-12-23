cb8b774a19e6cf1402324da802f2bc132a63a5e4
LeeKoldun <leadkoldun@gmail.com>
2022-12-23 13:24:47 +0300

    Фикс 2

--- RasArbitrCore/Model/ItemAnswerView.cs
+++ RasArbitrCore/Model/ItemAnswerView.cs
@@ -1,22 +1,20 @@
 ﻿using RasArbitrCore.API;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RasArbitrCore.Model {
-    public class ItemAnswerView {
+namespace RasArbitrCore.Model
+{
+    public class ItemAnswerView
+    {
         public Uri CaseId { get; set; } // url
         public Uri FileName { get; set; } //file url
         public string File { get; set; }
         public String Id { get; set; }
 
-        public string CourtName { get; set; }
+        public String CourtName { get; set; }
         public String Type { get; set; }
 
-        public ItemAnswerView(PostResult.ItemData Data) {
-            CaseId= new($"https://kad.arbitr.ru/Card/{Data.CaseId}");
+        public ItemAnswerView(PostResult.ItemData Data)
+        {
+            CaseId = new($"https://kad.arbitr.ru/Card/{Data.CaseId}");
             File = Data.FileName;
             FileName = new($"https://kad.arbitr.ru/Document/Pdf/{Data.CaseId}/{Data.Id}/{Data.FileName}");
             Id = Data.Id;
